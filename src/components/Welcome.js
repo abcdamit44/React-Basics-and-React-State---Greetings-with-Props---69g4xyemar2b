76aad6c5191abe99082f87500637bd7ccbf67a21
Amit Singh <abcdamit44@gmail.com>
2022-03-28 20:24:12 +0530

    Check 2

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -5,7 +5,7 @@ import Message from './Message'
 const Welcome = (props) => {
     return (
         <div>
-            <h1>Hey ! {props.name}</h1>
+            <h1>Hey {props.name}!</h1>
             <Message />
         </div>
     )
